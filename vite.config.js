5d7a65b1b57058a1e49fded64bfea3f0281da67f
Trent Avilla <131209117+vhalen0515@users.noreply.github.com>
2024-07-06 06:21:29 -0700

    test commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/tenzies-react-game"
+  base: "/Tenzies%20React%20Game/"
 })
